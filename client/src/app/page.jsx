6d729f6c79b98cb7eabd79b076d381c129f4a2e1
km-saifullah <kmsaifullah16@gmail.com>
2024-08-17 11:40:36 +0600

    header design done

--- client/src/app/page.jsx
+++ client/src/app/page.jsx
@@ -1,10 +1,3 @@
 export default function Home() {
-  return (
-    <main>
-      <h1 className="font-inter text-text">Hello World</h1>
-      <button className="btn">Hello</button>
-      <h2 className="main-heading"> Main Heading</h2>
-      <h1 className="hero-text">hero text Lorem ipsum dolor sit amet.</h1>
-    </main>
-  );
+  return <main></main>;
 }
---
+++ client/src/components/Header.jsx
@@ -0,0 +1,36 @@
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header className="py-3 bg-primary">
+      <div className="container">
+        <div className="flex items-end justify-between ">
+          <div className="flex items-center justify-center gap-x-2">
+            <p className="text-light font-normal leading-[21px] text-[14px] font-poppins">
+              Summer Sale For All Swim Suits And Free Express Delivery - OFF
+              50%!
+            </p>
+            <Link
+              href={"#"}
+              className="text-light font-semibold leading-6 text-[14px] font-poppins underline"
+            >
+              ShopNow
+            </Link>
+          </div>
+          <div>
+            <select
+              name="English"
+              id=""
+              className="bg-transparent border-none border-transparent text-light px-3 outline-none outline-transparent text-[14px] font-normal font-poppins leading-[21px]"
+            >
+              <option>English</option>
+              <option>Bangla</option>
+            </select>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
