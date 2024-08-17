150ae1bd0a68ca80c44cdbf68c25a2f3245d62a2
km-saifullah <kmsaifullah16@gmail.com>
2024-08-17 11:24:42 +0600

    tailwind configuration done

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -6,11 +6,36 @@ module.exports = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "320px",
+      md: "640px",
+      lg: "768px",
+      xl: "1040px",
+      "2xl": "1280px",
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        md: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      colors: {
+        primary: "#181818",
+        secondary: "#DB4444",
+        light: "#FAFAFA",
+        text: "rgba(0, 0, 0, 0.5)",
+        "light-green": "#00FF66",
+        "border-line": "rgba(0, 0, 0, 0.4)",
+      },
+      fontFamily: {
+        poppins: '"Poppins", sans-serif',
+        inter: '"Inter", sans-serif',
       },
     },
   },
