60af28cfff4648531ea0053d472f3bb911c29893
km-saifullah <kmsaifullah16@gmail.com>
2024-12-03 23:38:29 +0600

    show all products in the product page feature added

--- client/src/app/products/page.jsx
+++ client/src/app/products/page.jsx
@@ -1,8 +1,26 @@
+"use client";
+import { useEffect, useState } from "react";
+import axios from "axios";
 import Image from "next/image";
+import config from "@/config/config";
 import gamepad from "/public/usb_gamepad.png";
-import ps5 from "/public/ps5.png";
 
 const Products = () => {
+  const [products, setProducts] = useState([]);
+
+  const getProducts = async () => {
+    const productsUrl = `${config.baseUrl}/products`;
+    try {
+      const response = await axios.get(productsUrl);
+      setProducts(response.data);
+      console.log(response.data);
+    } catch (error) {}
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   return (
     <section>
       <div className="container">
@@ -10,181 +28,38 @@ const Products = () => {
           <div className="bg-productBanner w-full h-[316px] bg-no-repeat bg-cover bg-center"></div>
         </div>
         <div className="flex items-center justify-center flex-wrap gap-4 py-6">
-          <div className="bg-inputBg w-[250px] p-5 rounded-md">
-            <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
-              <Image
-                src={gamepad}
-                alt="product name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="space-y-1 pt-2">
-              <h2 className="text-base text-primary font-bold font-inter leading-6">
-                USB Gamepad
-              </h2>
-              <p className="text-text text-sm font-poppins font-normal leading-6">
-                Premium quality
-              </p>
-              <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
-                $350
-              </h3>
-              <div>
-                <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
-                  Add To Cart
-                </button>
+          {products?.data?.products?.map((item) => (
+            <div
+              key={item.slug}
+              className="bg-inputBg w-[250px] p-5 rounded-md"
+            >
+              <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
+                <Image
+                  src={item.thumbnail.thumnailImage || gamepad}
+                  alt="product name"
+                  className="w-full h-full object-cover"
+                  width={100}
+                  height={100}
+                />
+              </div>
+              <div className="space-y-1 pt-2">
+                <h2 className="text-base text-primary font-bold font-inter leading-6">
+                  {item.title}
+                </h2>
+                <p className="text-text text-sm font-poppins font-normal leading-6">
+                  Premium quality
+                </p>
+                <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
+                  {item.inventory[0].sellingPrice} BDT
+                </h3>
+                <div>
+                  <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
+                    Add To Cart
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-          <div className="bg-inputBg w-[250px] p-5 rounded-md">
-            <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
-              <Image
-                src={ps5}
-                alt="product name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="space-y-1 pt-2">
-              <h2 className="text-base text-primary font-bold font-inter leading-6">
-                PS5 Controller
-              </h2>
-              <p className="text-text text-sm font-poppins font-normal leading-6">
-                Premium quality
-              </p>
-              <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
-                $500
-              </h3>
-              <div>
-                <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="bg-inputBg w-[250px] p-5 rounded-md">
-            <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
-              <Image
-                src={gamepad}
-                alt="product name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="space-y-1 pt-2">
-              <h2 className="text-base text-primary font-bold font-inter leading-6">
-                USB Gamepad
-              </h2>
-              <p className="text-text text-sm font-poppins font-normal leading-6">
-                Premium quality
-              </p>
-              <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
-                $350
-              </h3>
-              <div>
-                <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="bg-inputBg w-[250px] p-5 rounded-md">
-            <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
-              <Image
-                src={gamepad}
-                alt="product name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="space-y-1 pt-2">
-              <h2 className="text-base text-primary font-bold font-inter leading-6">
-                USB Gamepad
-              </h2>
-              <p className="text-text text-sm font-poppins font-normal leading-6">
-                Premium quality
-              </p>
-              <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
-                $350
-              </h3>
-              <div>
-                <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="bg-inputBg w-[250px] p-5 rounded-md">
-            <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
-              <Image
-                src={gamepad}
-                alt="product name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="space-y-1 pt-2">
-              <h2 className="text-base text-primary font-bold font-inter leading-6">
-                USB Gamepad
-              </h2>
-              <p className="text-text text-sm font-poppins font-normal leading-6">
-                Premium quality
-              </p>
-              <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
-                $350
-              </h3>
-              <div>
-                <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="bg-inputBg w-[250px] p-5 rounded-md">
-            <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
-              <Image
-                src={gamepad}
-                alt="product name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="space-y-1 pt-2">
-              <h2 className="text-base text-primary font-bold font-inter leading-6">
-                USB Gamepad
-              </h2>
-              <p className="text-text text-sm font-poppins font-normal leading-6">
-                Premium quality
-              </p>
-              <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
-                $350
-              </h3>
-              <div>
-                <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-          </div>
-          <div className="bg-inputBg w-[250px] p-5 rounded-md">
-            <div className="w-[200px] h-[160px] overflow-hidden flex items-center justify-center">
-              <Image
-                src={gamepad}
-                alt="product name"
-                className="w-full h-full object-cover"
-              />
-            </div>
-            <div className="space-y-1 pt-2">
-              <h2 className="text-base text-primary font-bold font-inter leading-6">
-                USB Gamepad
-              </h2>
-              <p className="text-text text-sm font-poppins font-normal leading-6">
-                Premium quality
-              </p>
-              <h3 className="text-secondary font-semibold font-poppins text-base leading-6">
-                $350
-              </h3>
-              <div>
-                <button className="bg-secondary text-white text-base font-poppins font-normal leading-6 px-4 py-2 rounded">
-                  Add To Cart
-                </button>
-              </div>
-            </div>
-          </div>
+          ))}
         </div>
         <div className="flex items-center justify-center py-5 pb-9">
           <div className="join">
